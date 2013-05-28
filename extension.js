57cdbb8df3e09e93d949ec26da7a60cf65ad5afa
nemo <git@nemo.ikkoku.de>
2013-05-28 02:15:57 +0200

    Port to 3.8
    `imports.dbus` got replaced by `imports.gi.Gio` and its DBus methods.
    
    Also the signal handling changed as the parameters are not applied
    as native parameters but apparently passed as a list to the handler.
    
    Lastly, the interface definition changed from JSON to XML. Oh boy.

--- extension.js
+++ extension.js
@@ -1,21 +1,30 @@
 const Main = imports.ui.main;
 const Lang = imports.lang;
-const DBus = imports.dbus;
+const Gio = imports.gi.Gio;
 const Shell = imports.gi.Shell;
 
 let UserMenuButton = Main.panel.statusArea.userMenu;
 
-const PurpleIface = {
-    name: 'im.pidgin.purple.PurpleInterface',
-    properties: [],
-    methods: [],
-    signals: [
-        {name: 'DisplayedImMsg', inSignature: 'issii'},
-        {name: 'ConversationUpdated', inSignature: 'iu'}
-    ]
-};
+/**
+ * Mostly taken from the output of
+ * dbus-send --print-reply --dest=im.pidgin.purple.PurpleService \
+   /im/pidgin/purple/PurpleObject org.freedesktop.DBus.Introspectable.Introspect
+ */
+const PurpleIface = <interface name="im.pidgin.purple.PurpleInterface">
+    <signal name='DisplayedImMsg'>
+        <arg type='i'/>
+        <arg type='s'/>
+        <arg type='s'/>
+        <arg type='i'/>
+        <arg type='i'/>
+    </signal>
+    <signal name='ConversationUpdated'>
+      <arg type='i'/>
+      <arg type='u'/>
+    </signal>
+</interface>;
 
-let Purple = DBus.makeProxyClass(PurpleIface);
+const PurpleProxy = Gio.DBusProxy.makeProxyWrapper(PurpleIface);
 
 function PurpleClient() {
     this._init();
@@ -27,21 +36,47 @@ const PURPLE_MESSAGE_SYSTEM = 0x4;
 PurpleClient.prototype = {
 
     _init: function() {
-		// Nothing to initialize, everything is initialized in enable()
+        // All DBus signals are connected and need to be disconnected
+        // on disable().
+		this._signalsConnected = false;
     },
 
+    /**
+     * Pidgin just joined the bus. Connect signals and wait for action.
+     */
     _onPurpleAppeared: function(owner) {
-        this._proxy = new Purple(DBus.session,
+        if(this._signalsConnected) {
+            global.log('Pidgin appeared again. Reconnecting signals.');
+            this._disconnectFromPidgin();
+        }
+
+        this._signalsConnected = true;
+
+        this._proxy = new PurpleProxy(Gio.DBus.session,
 			'im.pidgin.purple.PurpleService', '/im/pidgin/purple/PurpleObject');
 
-        this._displayedImMessageId = this._proxy.connect('DisplayedImMsg',
+        this._displayedImMessageId = this._proxy.connectSignal('DisplayedImMsg',
 			Lang.bind(this, this._onDisplayedImMsg));
 
-        this._conversationUpdated = this._proxy.connect('ConversationUpdated',
+        this._conversationUpdatedId = this._proxy.connectSignal('ConversationUpdated',
 			Lang.bind(this, this._onConversationUpdated));
     },
 
-    _onDisplayedImMsg: function(emitter, _account, _who, _message, _conv, flags) {
+    /**
+     * Pidgin disappeared from the bus. We stop listening and wait for
+     * Pidgin to return.
+     */
+    _onPurpleDisappeared: function(owner) {
+        this._disconnectFromPidgin();
+    },
+
+    /**
+     * A message was shown in the message window. Display a persistent
+     * notification if the pidgin window is not focussed.
+     */
+    _onDisplayedImMsg: function(_emitter, _handleId, params) {
+        let [_account, _who, _message, _conv, flags] = params;
+
 		if(flags & PURPLE_MESSAGE_SYSTEM) {
 			return;
 		}
@@ -53,8 +88,14 @@ PurpleClient.prototype = {
         }
     },
 
-    _onConversationUpdated: function(emitter, _conv, flags) {
-        if(flags & PURPLE_CONV_UPDATE_UNSEEN) {
+    /**
+     * State of the conversation window has changed. Remove the persistent
+     * notification if the message was read.
+     */
+    _onConversationUpdated: function(_emitter, _handleId, params) {
+        let [_conv, flags] = params;
+
+        if(PURPLE_CONV_UPDATE_UNSEEN & flags) {
 			this._removePersistentNotification();
         }
     },
@@ -67,21 +108,50 @@ PurpleClient.prototype = {
 		UserMenuButton._iconBox.remove_style_class_name('pidgin-notification');
 	},
 
+    /**
+     * Disconnect the Pidgin signal listeners and remove any notification
+     * as there's no way to reset it after disconnecting.
+     */
+    _disconnectFromPidgin: function() {
+        if(!this._signalsConnected) {
+            return;
+        }
+
+        this._proxy.disconnectSignal(this._displayedImMessageId);
+        this._proxy.disconnectSignal(this._conversationUpdatedId);
+
+        this._removePersistentNotification();
+
+        this._signalsConnected = false;
+    },
+
+    /**
+     * Entry point of the extension.
+     *
+     * Setup handlers for when Pidgin joins, is already present on,
+     * or leaves the bus.
+     */
 	enable: function() {
-        DBus.session.watch_name('im.pidgin.purple.PurpleService', false,
-			Lang.bind(this, this._onPurpleAppeared), null);
+        this._purpleWatchId = Gio.DBus.session.watch_name(
+            'im.pidgin.purple.PurpleService',
+            Gio.BusNameWatcherFlags.NONE,
+            Lang.bind(this, this._onPurpleAppeared),
+            Lang.bind(this, this._onPurpleDisappeared)
+        );
 	},
 
 	disable: function() {
-		this._proxy.disconnect(this._displayedImMessageId);
-		this._proxy.disconnect(this._conversationUpdated);
+        if(!this._signalsConnected) {
+            return;
+        }
 
-		this._removePersistentNotification();
+        this._disconnectFromPidgin();
+
+        Gio.DBus.session.unwatch_name(this._purpleWatchId);
 	}
 }
+
 
 function init() {
 	return new PurpleClient();
 }
-
-
