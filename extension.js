f4738f35f49400c7720d9f4d8c1bf39bb3e07140
nemo <git@nemo.ikkoku.de>
2014-05-03 01:49:15 +0200

    #6: Re-format code, replacing whitespace with tabs

--- extension.js
+++ extension.js
@@ -39,90 +39,90 @@ const PersistentIndicator = new Lang.Class({
 	Extends: PanelMenu.Button,
 
 	_init: function() {
-        this.parent(0.0, "Chat Indicator");
+		this.parent(0.0, "Chat Indicator");
 
 		let icon = new St.Icon({
 			icon_name: 'user-available-symbolic',
 			style_class: 'system-status-icon'
 		});
 
-        this.actor.add_actor(icon);
-    },
+		this.actor.add_actor(icon);
+	},
 });
 
 
 function PurpleClient() {
-    this._init();
+	this._init();
 }
 
 PurpleClient.prototype = {
 
-    _init: function() {
-        // All DBus signals are connected and need to be disconnected
-        // on disable().
+	_init: function() {
+		// All DBus signals are connected and need to be disconnected
+		// on disable().
 		this._signalsConnected = false;
 
 		this._indicator = null;
-    },
+	},
 
-    /**
-     * Pidgin just joined the bus. Connect signals and wait for action.
-     */
-    _onPurpleAppeared: function(owner) {
-        if(this._signalsConnected) {
-            global.log('Pidgin appeared again. Reconnecting signals.');
-            this._disconnectFromPidgin();
-        }
+	/**
+	 * Pidgin just joined the bus. Connect signals and wait for action.
+	 */
+	_onPurpleAppeared: function(owner) {
+		if(this._signalsConnected) {
+			global.log('Pidgin appeared again. Reconnecting signals.');
+			this._disconnectFromPidgin();
+		}
 
-        this._proxy = new PurpleProxy(Gio.DBus.session,
+		this._proxy = new PurpleProxy(Gio.DBus.session,
 			'im.pidgin.purple.PurpleService', '/im/pidgin/purple/PurpleObject');
 
-        this._displayedImMessageId = this._proxy.connectSignal('DisplayedImMsg',
+		this._displayedImMessageId = this._proxy.connectSignal('DisplayedImMsg',
 			Lang.bind(this, this._onDisplayedImMsg));
 
-        this._conversationUpdatedId = this._proxy.connectSignal('ConversationUpdated',
+		this._conversationUpdatedId = this._proxy.connectSignal('ConversationUpdated',
 			Lang.bind(this, this._onConversationUpdated));
 
 		this._signalsConnected = true;
-    },
+	},
 
-    /**
-     * Pidgin disappeared from the bus. We stop listening and wait for
-     * Pidgin to return.
-     */
-    _onPurpleDisappeared: function(owner) {
-        this._disconnectFromPidgin();
-    },
+	/**
+	 * Pidgin disappeared from the bus. We stop listening and wait for
+	 * Pidgin to return.
+	 */
+	_onPurpleDisappeared: function(owner) {
+		this._disconnectFromPidgin();
+	},
 
-    /**
-     * A message was shown in the message window. Display a persistent
-     * notification if the pidgin window is not focussed.
-     */
-    _onDisplayedImMsg: function(_emitter, _handleId, params) {
-        let [_account, _who, _message, _conv, flags] = params;
+	/**
+	 * A message was shown in the message window. Display a persistent
+	 * notification if the pidgin window is not focussed.
+	 */
+	_onDisplayedImMsg: function(_emitter, _handleId, params) {
+		let [_account, _who, _message, _conv, flags] = params;
 
 		if(flags & PURPLE_MESSAGE_SYSTEM) {
 			return;
 		}
 
-        let focusApp = Shell.WindowTracker.get_default().focus_app;
+		let focusApp = Shell.WindowTracker.get_default().focus_app;
 
-        if(focusApp == null || focusApp.get_id() != 'pidgin.desktop') {
+		if(focusApp == null || focusApp.get_id() != 'pidgin.desktop') {
 			this._addPersistentNotification();
-        }
-    },
+		}
+	},
 
-    /**
-     * State of the conversation window has changed. Remove the persistent
-     * notification if the message was read.
-     */
-    _onConversationUpdated: function(_emitter, _handleId, params) {
-        let [_conv, flags] = params;
+	/**
+	 * State of the conversation window has changed. Remove the persistent
+	 * notification if the message was read.
+	 */
+	_onConversationUpdated: function(_emitter, _handleId, params) {
+		let [_conv, flags] = params;
 
-        if(PURPLE_CONV_UPDATE_UNSEEN & flags) {
+		if(PURPLE_CONV_UPDATE_UNSEEN & flags) {
 			this._removePersistentNotification();
-        }
-    },
+		}
+	},
 
 	_addPersistentNotification: function() {
 		this._indicator.actor.add_style_class_name('pidgin-notification');
@@ -132,37 +132,37 @@ PurpleClient.prototype = {
 		this._indicator.actor.remove_style_class_name('pidgin-notification');
 	},
 
-    /**
-     * Disconnect the Pidgin signal listeners and remove any notification
-     * as there's no way to reset it after disconnecting.
-     */
-    _disconnectFromPidgin: function() {
-        if(!this._signalsConnected) {
-            return;
-        }
+	/**
+	 * Disconnect the Pidgin signal listeners and remove any notification
+	 * as there's no way to reset it after disconnecting.
+	 */
+	_disconnectFromPidgin: function() {
+		if(!this._signalsConnected) {
+			return;
+		}
 
-        this._proxy.disconnectSignal(this._displayedImMessageId);
-        this._proxy.disconnectSignal(this._conversationUpdatedId);
+		this._proxy.disconnectSignal(this._displayedImMessageId);
+		this._proxy.disconnectSignal(this._conversationUpdatedId);
 		this._proxy = null;
 
-        this._removePersistentNotification();
+		this._removePersistentNotification();
 
-        this._signalsConnected = false;
-    },
+		this._signalsConnected = false;
+	},
 
-    /**
-     * Entry point of the extension.
-     *
-     * Setup handlers for when Pidgin joins, is already present on,
-     * or leaves the bus.
-     */
+	/**
+	 * Entry point of the extension.
+	 *
+	 * Setup handlers for when Pidgin joins, is already present on,
+	 * or leaves the bus.
+	 */
 	enable: function() {
-        this._purpleWatchId = Gio.DBus.session.watch_name(
-            'im.pidgin.purple.PurpleService',
-            Gio.BusNameWatcherFlags.NONE,
-            Lang.bind(this, this._onPurpleAppeared),
-            Lang.bind(this, this._onPurpleDisappeared)
-        );
+		this._purpleWatchId = Gio.DBus.session.watch_name(
+			'im.pidgin.purple.PurpleService',
+			Gio.BusNameWatcherFlags.NONE,
+			Lang.bind(this, this._onPurpleAppeared),
+			Lang.bind(this, this._onPurpleDisappeared)
+		);
 
 		this._indicator = new PersistentIndicator;
 
@@ -174,7 +174,7 @@ PurpleClient.prototype = {
 			this._indicator.destroy();
 		}
 
-        if(this._signalsConnected) {
+		if(this._signalsConnected) {
 			return;
 		}
 
